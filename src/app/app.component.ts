7ef01810dc25ef95162a4c57f8287cc3a6139d1a
edsonjunior1 <edson.ascendinojr@gmail.com>
2022-12-26 23:18:54 -0300

    First Commit with half project

---
+++ src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { AfterViewInit, Component, ElementRef, ViewChild, ViewChildren, QueryList } from '@angular/core';
+import { COURSES } from '../db-data';
+import { Course } from './model/course';
+import { CourseCardComponent } from './course-card/course-card.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterViewInit {
+
+  // @ViewChild('course') elementReference!: ElementRef;
+  @ViewChild('container') containerDiv!: ElementRef;
+
+  //Query List dos elementos, elementos do DOM, assim como o viewchild não pega outros componentes
+  @ViewChildren(CourseCardComponent, {read: ElementRef})
+  cards!: QueryList<ElementRef>;
+
+  courses = COURSES;
+  course = COURSES[0];
+  _id: number = this.course.lenght;
+
+
+  constructor(){}
+
+//PAREI ANTES DE VER VIEWCHIELDREN
+
+  ngAfterViewInit(): void {
+    //Only local in component
+    this.containerDiv.nativeElement;
+  }
+
+
+  onCourseEmit(course: Course) {
+    console.log(`Objeto destructured which the description is: ${course.description}`);
+    // this.elementReference.nativeElement.className = 'alert alert-success';
+  }
+
+  onCourseEmited(){
+
+    for (var i = 0; i < this.course.lenght; i++) {
+      this._id = this._id + 1;
+    }
+
+    this.courses.push({
+        id: this._id,
+        description: "New Course Added!",
+        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+        longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
+        category: 'INTERMEDIATE',
+        lessonsCount: 10
+    });
+  }
+}
---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,54 @@
+import { Component, EventEmitter, Input, OnInit, Output, AfterViewInit, ContentChild, ElementRef, ViewChildren, QueryList, ContentChildren, AfterContentInit, TemplateRef } from '@angular/core';
+import { Course } from '../model/course';
+import { CourseImageComponent } from '../course-image/course-image.component';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent implements OnInit, AfterViewInit, AfterContentInit {
+
+  @Input() course: Course = { id: 0, category: '', description: '', iconUrl: '', longDescription: '', lessonsCount: 0 };
+  @Input() cardIndex: number = 0;
+  @Output() courseSelected = new EventEmitter<Course>();
+
+  //Adicionando a referencia do template noImage
+  @Input()
+  noImageTemplate!: TemplateRef<any>;
+
+  @ContentChild(CourseImageComponent, {read: ElementRef})
+  image!: ElementRef;
+
+  @ContentChildren(CourseImageComponent)
+  images!:QueryList<CourseImageComponent>
+
+  // In order to see the query results, we need to use @ContentChild, so that we can only inside the ng-scontent
+  constructor() { }
+
+  ngOnInit() { }
+
+  ngAfterViewInit(){
+    console.log(this.image);
+  }
+
+  ngAfterContentInit(): void {
+    console.log(this.images);
+  }
+
+  onCourseViewed() {
+    this.courseSelected.emit(this.course);
+  }
+
+  isImageAvailable() {
+    return this.course && this.course.iconUrl
+  }
+
+  cardClasses():any {
+    if (this.course.category == 'BEGINNER') {
+      return 'beginner';
+    }
+  }
+
+
+}
